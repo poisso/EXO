425c7e5d93d8d8794e863a0019f3e2479bc34b34
Avo-k <66020779+Avo-k@users.noreply.github.com>
2021-04-12 20:34:18 +0200

    Add files via upload

--- translator/views.py
+++ translator/views.py
@@ -47,9 +47,8 @@ def result(request):
               output_suffix="\n\n",
               append_output_prefix_to_query=True)
 
-    inputs = [examples[i][:-1] for i in range(0, 12, 3)]
-    outputs = [examples[i][:-1] for i in range(1, 11, 3)]
-    j = 0
+    inputs = [examples[i][:-1] for i in range(0, 8, 2)]
+    outputs = [examples[i][:-1] for i in range(1, 9, 2)]
 
     for i in range(4):
         gpt.add_example(Example(inputs[i], outputs[i]))
